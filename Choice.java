b3179422da639443a94a893ca8ca78324727f92d
slmccarthy <slmccarthy@loyola.edu>
2020-10-13 12:40:52 -0400

    Added choice

---
+++ Choice.java
@@ -0,0 +1,11 @@
+/* Class choice: interpret  choice by user
+ * CS312 Lab6
+ * @authors Shane McCarthy, Joseph Carnovale
+ * @version 1.0 10/13/20
+ */
+
+public abstract class Choice
+{
+	public abstract void beats(Choice x);
+
+}
